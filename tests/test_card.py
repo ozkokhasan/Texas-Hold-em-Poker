3d05033a6905154957b9a21c09619a2280e378f3
Hasan Özkök <hasan.ozkok@akinon.com>
2021-08-09 20:03:47 +0300

    crated Card class

---
+++ tests/test_card.py
@@ -0,0 +1,19 @@
+import unittest
+from poker.card import Card
+
+class CardTest(unittest.TestCase):
+    def test_card_has_rank(self):
+        card = Card(rank = "Queen", suit = "Hearts")
+        self.assertEqual(card.rank, "Queen")
+    
+    def test_card_has_suit(self):
+        card = Card(rank = "2", suit = "Clubs")
+        self.assertEqual(card.suit, "Clubs")
+
+    def test_card_has_string_representation_with_rank_and_suit(self):
+        card = Card("5", "Diamonds")
+        self.assertEqual(str(card), "5 of Diamonds")
+
+    def test_card_has_technical_representation(self):
+        card = Card("5", "Diamonds")
+        self.assertEqual(repr(card), "Card('5', 'Diamonds')")
