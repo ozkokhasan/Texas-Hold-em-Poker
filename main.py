5ff7f716cd46e712946f8b61d3b92da8b8c5b4bb
Hasan Özkök <hasan.ozkok@akinon.com>
2021-08-10 12:45:54 +0300

    added create 52 cards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from poker.card import Card
+from poker.deck import Deck
 
-card1 = Card(rank = "2", suit = "Spades")
-card2 = Card(rank = "Ace", suit = "Hearts")
-
-# from main import card1, card2
+deck = Deck()
+cards = Card.create_standart_52_cards()
+deck.add_cards(cards)
+# from main import deck, cards
