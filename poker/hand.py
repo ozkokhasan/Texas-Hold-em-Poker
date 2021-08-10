99df4d7e85591c3a974d1501e4128d7418bec5ff
Hasan Özkök <hasan.ozkok@akinon.com>
2021-08-10 14:31:03 +0300

    refoctered hand class

--- poker/hand.py
+++ poker/hand.py
@@ -2,21 +2,49 @@ class Hand():
 
     def __init__(self, cards):
         self.cards = cards
+
+    @property
+    def _rank_validations_from_best_to_worst(self):
+        return (
+            ("Three of a Kind", self._three_of_a_kind),
+            ("Two Pair", self._two_pair),
+            ("Pair", self._pair),
+            ("High Card", self._high_card)
+        )
+    def best_rank(self):
+        for rank in self._rank_validations_from_best_to_worst:
+            name, validator_method = rank
+            if validator_method():
+                return name
+
+        # ranks_with_three_of_kind = self._ranks_with_count(3)
+        # if len(ranks_with_three_of_kind) == 1:
+        #     return "Three of a Kind"
+
+        # ranks_with_pairs = self._ranks_with_count(2)
+        # if len(ranks_with_pairs) == 2:
+        #     return "Two Pair"
+
+        # if len(ranks_with_pairs) == 1:
+        #     return "Pair"
+
+        # return "High Card"
     
-    def best_rank(self):  
+    def _three_of_a_kind(self):
         ranks_with_three_of_kind = self._ranks_with_count(3)
-        if len(ranks_with_three_of_kind) == 1:
-            return "Three of a Kind"
-
-        ranks_with_pairs = self._ranks_with_count(2)
-        if len(ranks_with_pairs) == 2:
-            return "Two Pair"
-
-        if len(ranks_with_pairs) == 1:
-            return "Pair"
-
-        return "High Card"
+        return len(ranks_with_three_of_kind) == 1
     
+    def _two_pair(self):
+        ranks_with_pairs = self._ranks_with_count(2)
+        return len(ranks_with_pairs) == 2
+        
+    def _pair(self):
+        ranks_with_pairs = self._ranks_with_count(2)
+        return len(ranks_with_pairs) == 1
+
+    def _high_card(self):
+        return True
+
     def _ranks_with_count(self, count):
         return {
             rank: rank_count
