87df7add923595bbaf96849c0fc78b53dcf1eadf
Hasan Özkök <hasan.ozkok@akinon.com>
2021-08-10 10:33:04 +0300

    created base deck class

---
+++ tests/test_deck.py
@@ -0,0 +1,20 @@
+import unittest
+from poker.deck import Deck
+from poker.card import Card
+
+class DeckTest(unittest.TestCase):
+    def test_stores_no_cards_at_start(self):
+        deck = Deck()
+        self.assertEqual(
+            deck.cards,
+            []
+        )
+    
+    def test_add_cards_to_its_collection(self):
+        card = Card(rank = "Ace", suit = "Spades")
+        deck = Deck()
+        deck.add_cards([card])
+        self.assertEqual(
+            deck.cards,
+            [card]
+        )
