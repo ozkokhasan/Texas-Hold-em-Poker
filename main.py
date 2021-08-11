166978b91caf754b9c285e64ec92fe4f9db1261c
Hasan Özkök <hasan.ozkok@akinon.com>
2021-08-12 01:11:52 +0300

    updated main file

--- main.py
+++ main.py
@@ -18,9 +18,13 @@ players = [player1, player2]
 game_round = GameRound(deck = deck, players = players)
 game_round.play()
 
-print(player1.hand)
-print(player1.best_hand())
-print(player2.hand)
-print(player2.best_hand())
+for player in players:
+    print(f"{player.name} receives a {player.hand}")
+    index, hand_name, hand_cards = player.best_hand()
+    hand_cards_strings = [str(card) for card in hand_cards]
+    hand_cards_string = " and ".join(hand_cards_strings)
+    print(f"{player.name} has a {hand_name} with a {hand_cards_strings}.")
 
-# from main import deck, cards, hand1, hand2, player1, player2, game_round
+winning_player = max(players)
+
+print(f"The winner is {winning_player.name}.")
